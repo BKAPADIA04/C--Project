29cfe5167003cff96a84388676a85c0c85f1da25
BKAPADIA04 <116899053+BKAPADIA04@users.noreply.github.com>
2023-09-20 18:30:21 +0530

    Update interpreter.h

--- include/interpreter.h
+++ include/interpreter.h
@@ -1,13 +1,13 @@
 #ifndef INTERPRETER_H
 #define INTERPRETER_H
 
-struct data
+typedef struct data
 {
     char *var_name;
     int *value;
-};
+}entry;
 
-extern struct data s[1000];
+extern entry data[1000];
 void execute_c_minus_minus(char *code);
 
 #endif /* INTERPRETER_H */
